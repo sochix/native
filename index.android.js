6e3aedce045651a107051dfd89742fdc193d6fe8
Ilya P <ilya@github.com>
2016-12-04 18:03:43 +0300

    Initial version of LessPass

--- index.android.js
+++ index.android.js
@@ -9,28 +9,121 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
-import { Button } from 'react-native-elements'
+  View,
+  Alert,
+  Clipboard,
+  KeyboardAvoidingView
+} from 'react-native'
+
+import nextFrame from 'next-frame';
+import {
+  Button, SocialIcon, Icon, FormLabel, FormInput
+} from 'react-native-elements'
+
+import {encryptLogin, renderPassword, createFingerprint} from './core/index'
+
+var site = 'lesspass.com';
+var login = 'contact@lesspass.com';
+var masterPassword = 'password';
+var options = {
+    counter: 1,
+    length: 12,
+    lowercase: true,
+    uppercase: true,
+    numbers: true,
+    symbols: true,
+    template: 'vcVCns'
+};    
 
 export default class phone extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = { 
+          generatedPass: '',
+          login: '',
+          site: '',
+          masterPass: '',
+          generating: false
+        }
+  }
+
   render() {
+
+    const onPress = () => {              
+       let encryptedLogin = ''
+       
+      this.setState({ ...this.state, generating: true})
+
+      nextFrame()
+        .then(() => encryptLogin(this.state.login, this.state.masterPassword))  
+        .then(result => {          
+          encryptedLogin = result.toString()
+
+          return renderPassword(encryptedLogin, this.state.site, options)
+                .then(generatedPassword => {
+                    this.setState({ 
+                      generatedPass: generatedPassword,
+                      generating: false
+                    })  
+                })
+        })
+    }
+
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to LessPass!
-        </Text>
-        <Button
-          raised
-          icon={{name: 'cached'}}
-          title='RAISED WITH ICON' />
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={styles.container}>       
+        <View style={styles.headerViewStyle}>
+          <Icon name='vpn-key' size={40} iconStyle={styles.headerIconStyle}/>
+          <Text style={{fontSize: 40, color: 'white'}}>LessPass</Text>
+        </View>
+        <KeyboardAvoidingView behaviour='padding' style={styles.avoidingViewStyle}> 
+          <View style={styles.inputView} >          
+            <FormInput
+              inputStyle={styles.inputStyle}
+              placeholder={'Site'}
+              keyboardType={'url'}
+              onChangeText={(value) => this.setState({...this.state, site: value})}
+              value={this.state.site}
+              />          
+            <FormInput
+              inputStyle={styles.inputStyle}
+              placeholder={'Login'}
+              autoCapitalize={'none'}
+              autoCorrect ={false}
+              onChangeText={(value) => this.setState({...this.state, login: value})}
+              value={this.state.login}
+              />          
+            <FormInput
+              inputStyle={styles.inputStyle}
+              placeholder={'Master password'}
+              autoCapitalize={'none'}
+              autoCorrect = {false}
+              secureTextEntry = {true}
+              onChangeText={(value) => this.setState({...this.state, masterPassword: value})}
+              value={this.state.masterPassword}
+            />
+        </View>
+        <View style={styles.generatedPassView}>
+            <FormInput
+              inputStyle={styles.generatedPassInputStyle}
+              placeholder={'Generated Pass'}
+              editable={false}
+              value={this.state.generatedPass}
+              />            
+        </View>
+       </KeyboardAvoidingView>
+       <View style={styles.generateButtonStyle}>
+          <Button   
+            onPress = {() => onPress()}      
+            icon={{ name: 'cached', color: 'grey' }}
+            color='grey'
+            backgroundColor='white'
+            fontSize={20}
+            borderRadius={7}
+            disabled={this.state.generating}
+            title={this.state.generating ? 'In process....' : 'GENERATE'} />
+          </View>        
       </View>
     );
   }
@@ -39,20 +132,61 @@ export default class phone extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#008ed6'    
+  },
+  headerViewStyle: {            
+    flex: 1,      
+    paddingTop: 25,
+    paddingBottom: 10,
+    flexDirection: 'row',    
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'    
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  headerIconStyle: {
+    color: 'white',
+    transform: [{rotate: '90deg'}]
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  labelStyle: {
+    fontSize: 30,
   },
+  inputView: {
+    flex: 2,    
+    flexDirection: 'column',
+    justifyContent: 'space-around' 
+  },
+  inputStyle: { 
+    paddingLeft: 10,       
+    fontSize: 25,    
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 7,
+    textDecorationLine: 'none',
+    height: 50
+  },  
+  generatedPassInputStyle: {
+    paddingLeft: 10,       
+    fontSize: 25,    
+    backgroundColor: '#eceeef',
+    borderColor: '#eceeef',
+    color: '#55595c',
+    borderWidth: 1,
+    borderRadius: 7,
+    textDecorationLine: 'none',
+    height: 50        
+  },
+  generatedPassView: {
+    flex: 1,  
+    justifyContent: 'center'    
+  },
+  generateButtonStyle: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingBottom: 25    
+  },
+  avoidingViewStyle: {
+    flex: 2
+  }
 });
 
 AppRegistry.registerComponent('phone', () => phone);
